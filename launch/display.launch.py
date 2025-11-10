97d7955051a57a0b767e9c8005f2a2b2f6ca40b8
Missiler <albinrik@gmail.com>
2025-11-10 17:28:04 +0100

    Finally able to map a room using SLAM where map - odom - base_link.

--- launch/display.launch.py
+++ launch/display.launch.py
@@ -6,7 +6,51 @@ from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 from ros_gz_bridge.actions import RosGzBridge
 from ros_gz_sim.actions import GzServer
+from launch.conditions import IfCondition, UnlessCondition
+from nav2_common.launch import RewrittenYaml
 import os
+
+
+'''
+Idag: 
+-----------------------------------------------------------------------------------------------------------------------------------
+Jag gjort så att det går att updatera kartan:
+I Terminalen:
+
+1: ros2 launch slam_toolbox online_async_launch.py use_sim_time:=true
+2: ros2 launch nav2_bringup navigation_launch.py use_sim_time:=true
+
+I Rviz:
+
+1: Add Panel -> Map
+I map: Välj antingen Global_costmp eller Local_costmap med fixed frame på map.
+Hitta markers:
+Sätt fixed frame på Odom och marker genom by Topic
+-----------------------------------------------------------------------------------------------------------------------------------
+
+Imorgon:
+-----------------------------------------------------------------------------------------------------------------------------------
+Skapa ett rum genom att använda tutorialen given av Articulated Robotics i hur man skapar sin egen värld på Gazebo.
+Använd Det rummet som har skapats på Gazebo för att mappa rummet med SLAM.
+MHA: Skapade kartan, implementera en planner så att det går att se en skapad rutt. --> Våran planner är:
+                                https://github.com/ros-navigation/navigation2/tree/main/nav2_smac_planner
+
+Hitta även ett sätt att kontrollera bilen istället för att skriva rakt in i terminalen. Kolla om det går att ändra själva filen så att
+topicen som den skrivs i matchar det vi har i cmd/vel. Y'know.
+
+------------------------------------------------------------------------------------------------------------------------------------
+
+I framtiden:
+-----------------------------------------------------------------------------------------------------------------------------------
+Hitta en controller som styr själva bilen.
+Blås hela Pi:en
+Fixa Docker så att processeringen går på datorn istället för Pi:en
+Överför så att det funkar på bilen genom att kolla på vilka topics som controllern publishar/Subscribar till -> Konvertera till bil
+-----------------------------------------------------------------------------------------------------------------------------------
+'''
+
+
+
 
 def generate_launch_description():
     pkg_share = get_package_share_directory('porsche')
@@ -17,7 +61,7 @@ def generate_launch_description():
     default_rviz_config_path = os.path.join(pkg_share, 'rviz', 'config.rviz')
     bridge_config_path = os.path.join(pkg_share, 'config', 'bridge_config.yaml')
     world_path = os.path.join(pkg_share, 'world', 'my_world.sdf')
-
+    
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -49,15 +93,6 @@ def generate_launch_description():
         create_own_container='False',
         use_composition='True',
     )
-    
-    odom_tf = Node(
-        package="tf2_ros",
-        executable="static_transform_publisher",
-        name="odom_to_base_link",
-        arguments=["0", "0", "0", "0", "0", "0", "odom", "base_link"],
-        parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')}]
-    )
-
 
     spawn_entity = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(gz_spawn_model_launch_source),
@@ -68,25 +103,33 @@ def generate_launch_description():
             'z': '0.65',
         }.items(),
     )
+
+    odom_tf = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        name="odom_to_base_link",
+        arguments=["0", "0", "0", "0", "0", "0", "odom", "base_link"],
+        parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')}]
+    )
+    
     ekf_node = Node(
         package='robot_localization',
         executable='ekf_node',
-        name='ekf_filter_node',
+        name='ekf_node',
         output='screen',
-        parameters=[os.path.join(pkg_share, 'config', 'ekf.yaml')],
+        parameters=[os.path.join(pkg_share, 'config', 'ekf.yaml')]
     )
-
 
     return LaunchDescription([
         DeclareLaunchArgument('use_sim_time', default_value='True'),
         DeclareLaunchArgument('model', default_value=default_model_path),
         DeclareLaunchArgument('rvizconfig', default_value=default_rviz_config_path),
         ExecuteProcess(cmd=['gz', 'sim', '-g'], output='screen'),
+        robot_state_publisher_node,
+        rviz_node,
         gz_server,
         ros_gz_bridge,
         spawn_entity,
-        robot_state_publisher_node,
         ekf_node,
-        odom_tf,
-        rviz_node,
+        #odom_tf,
     ])
